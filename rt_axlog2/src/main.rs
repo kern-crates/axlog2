695075fcc9d58deca06b1e52f4b6717b70928d4a
Shi Lei <shi_lei@massclouds.com>
2024-09-03 03:39:54 +0000

    add rt_axlog2

---
+++ rt_axlog2/src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate axlog2;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "Rust" fn runtime_main(_cpu_id: usize, _dtb_pa: usize) {
+    axlog2::init("debug");
+    info!("[rt_axlog2]: ...");
+    info!("[rt_axlog2]: ok!");
+    axhal::misc::terminate();
+}
+
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    arch_boot::panic(info)
+}
